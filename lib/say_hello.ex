085f1d5dbca7c9a0bdb6a05d17e60ba0987d09cc
Rusydy <rusydy.muhiddin@mekari.com>
2025-08-14 19:11:28 +0700

    Add Hellouer module with say_hello function

---
+++ lib/say_hello.ex
@@ -0,0 +1,16 @@
+defmodule Hellouer do
+  @doc """
+  Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+  Example:
+
+  ```
+  ['John', 'Smith'], 'Phoenix', 'Arizona'
+  ```
+
+  This example will return the string `Hello, John Smith! Welcome to Phoenix, Arizona!`
+  """
+  def say_hello(name, city, state) do
+    "Hello, #{Enum.join(name, " ")}! Welcome to #{city}, #{state}!"
+  end
+end

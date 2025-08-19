7d25f13961508f32c9e13c88329dd1ac2025812f
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2025-08-20 04:42:47 +0700

    Add CodeValidator module with valid?/1 function

---
+++ lib/valid.ex
@@ -0,0 +1,12 @@
+defmodule CodeValidator do
+  @doc """
+  Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+  You can assume the input will always be a number.
+  """
+  def valid?(code) do
+    code
+    |> Integer.to_string()
+    |> String.starts_with?(["1", "2", "3"])
+  end
+end

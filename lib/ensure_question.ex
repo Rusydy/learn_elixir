a1c91bce516d1c8d693f57e78ec59659328adee3
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2025-08-20 03:33:39 +0700

    Add ensure_question/1 to append question mark if missing

---
+++ lib/ensure_question.ex
@@ -0,0 +1,17 @@
+defmodule Parsers do
+  @doc """
+  Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+  For example (Input --> Output)
+
+  "Yes" --> "Yes?"
+  "No?" --> "No?"
+  """
+  def ensure_question(s) do
+    if String.ends_with?(s, "?") do
+      s
+    else
+      s <> "?"
+    end
+  end
+end
